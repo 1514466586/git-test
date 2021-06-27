5ffdf451b06d8219743512fb97661ae9480eff72
郑云锋 <zhengyunfeng7@gmail.com>
2021-06-27 12:42:59 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,13 @@
+package git;
+
+/**
+ * @className: GitTest
+ * @description: TODO 类描述
+ * @author: 15144
+ * @date: 2021/6/27
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
